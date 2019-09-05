cc5d27a5cc5149f23f3c9fb650fb4dbc54e990a1
Frank Liao <frank@frankliao.com>
2019-09-04 22:20:03 -0700

    Completed 31 - Hot Reload and Hot Restart - Flutter Power Tools

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.white,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
