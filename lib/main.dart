d0e371c0aeb2c5cb4c8c0f681bf3d7d6818fc7df
edilson14 <edytavares66@gmail.com>
2020-02-10 22:21:35 -0300

    rows and coluns layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,35 @@ class MiCard extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Hello"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {},
-          child: Icon(Icons.call_made),
+        backgroundColor: Colors.lightBlue,
+        body: SafeArea(
+          child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 100.0,
+                  color: Colors.black,
+                ),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        height: 100.0,
+                        width: 100.0,
+                        color: Colors.lightGreenAccent,
+                      ),
+                      Container(
+                        height: 100.0,
+                        width: 100.0,
+                        color: Colors.yellowAccent,
+                      )
+                    ]),
+                Container(
+                  width: 100.0,
+                  color: Colors.red,
+                ),
+              ]),
         ),
       ),
     );
