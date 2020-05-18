f7921de390cab651e95343a1b24e7bf8bf9174f5
Mukaila Semiu <semiteagain@gmail.com>
2020-05-18 11:18:55 +0100

    add changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return   MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(child: Column(
+         children: <Widget>[
+           CircleAvatar(
+             radius: 50.0,
+             backgroundImage: AssetImage('images/semiu.jpg'),
+           ),
+         ],
+        ),)
       ),
-    ),
-  );
+    );
+  }
 }
