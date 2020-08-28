ecb932100b7441d3c26db2e2ca7da7ad01cf7280
Wasiu Idowu <hoxtygen@live.com>
2020-08-28 08:21:17 +0100

    Update to show avatar and title

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,34 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage("images/download-cors.PNG"),
+                  radius: 50.0,
+                ),
+                Text(
+                  "Idowu Wasiu",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Pacifico"),
+                ),
+                Text(
+                  "Software Developer".toUpperCase(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Pacifico",
+                  ),
+                ),
+              ],
+            ),
+          )),
     ),
   );
 }
