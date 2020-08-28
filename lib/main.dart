426b869e23c76d0001fcb0219474479395b2e268
Wasiu Idowu <hoxtygen@live.com>
2020-08-28 09:46:56 +0100

    feat: Add contact
    Add means of getting contacted.

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,20 @@ void main() {
           backgroundColor: Colors.teal,
           body: SafeArea(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CircleAvatar(
                   backgroundImage: AssetImage("images/download-cors.PNG"),
-                  radius: 50.0,
+                  radius: 70.0,
                 ),
                 Text(
                   "Idowu Wasiu",
                   style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "Pacifico"),
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Pacifico",
+                  ),
                 ),
                 Text(
                   "Software Developer".toUpperCase(),
@@ -26,9 +28,71 @@ void main() {
                     color: Colors.white,
                     fontSize: 20.0,
                     fontWeight: FontWeight.bold,
-                    fontFamily: "Pacifico",
+                    fontFamily: "Source Sans Pro",
+                    letterSpacing: 2.5,
                   ),
                 ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "Hoxtygen@live.com",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: "Source Sans Pro",
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "+234 806 018 4972",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: "Source Sans Pro",
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone_android,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "twitter.com/hoxtygen",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: "Source Sans Pro",
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
           )),
