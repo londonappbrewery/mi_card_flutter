d4706bb61c2d56bb76ecb18ed968852dbbd0080c
Shayan Ahmad <v-shahmad@hotels.com>
2020-10-09 02:52:58 +0200

    FlutterLearning: Completed login (dummy) page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/shayan.jpg'),
+              ),
+              Text(
+                'Shayan Ahmad',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.white,
+                  letterSpacing: 4.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+36 20 1234 567',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal[900],
+                          fontFamily: 'SourceSansPro'),
+                    )),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    title: Text(
+                      'shayan@email.com',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal[900],
+                          fontFamily: 'SourceSansPro'),
+                    ),
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                  )),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
