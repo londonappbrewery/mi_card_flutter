231f0bfb0e251fd20925765b45d521fc1f810d36
Sankate Sharma <sankate@sankate.com>
2020-10-16 14:53:33 -0700

    Upgrading pre 1.12 android project to new version using https://github.com/flutter/flutter/wiki/Upgrading-pre-1.12-Android-projects

--- android/app/src/main/java/co/appbrewery/micard/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/micard/MainActivity.java
@@ -1,13 +1,9 @@
 package co.appbrewery.micard;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+
 }
