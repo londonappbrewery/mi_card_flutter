602a851332ccda876f8cf05444e156fe06ccbf07
DJ Anniss <annissdj@gmail.com>
2021-03-07 13:18:19 +1300

    converting from android studio

---
+++ android/app/src/main/kotlin/co/appbrewery/mi_card_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.appbrewery.mi_card_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                child: Text('Container 1'),
+                width: 100.0,
+                height: 100.0,
+                // margin: EdgeInsets.only(left: 30.0),
+                // padding: EdgeInsets.all(20.0),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+              Container(
+                width: double.infinity,
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
