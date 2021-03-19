77d84b68d77937a7f8e5a30ca9d6972536f75baf
xtechnologytr <xtechnologytr@gmail.com>
2021-03-19 03:50:49 +0300

    Flutter Denemeleri V-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,55 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: SizedBox(
+          height: 950,
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 750.0,
+                width: 100.0,
+                color: Colors.redAccent[700],
+                child: Center(
+                  child: Text('Red'),
+                ),
+              ),
+              SizedBox(
+                width: 40.0,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.limeAccent,
+                child: Center(
+                  child: Text('Yellow'),
+                ),
+              ),
+              SizedBox(
+                width: 50.0,
+              ),
+              Container(
+                width: 100.0,
+                height: 750.0,
+                color: Colors.blue,
+                child: Center(
+                  child: Text('Blue'),
+                ),
+              ),
+            ],
+          ),
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
