489cbbf01e92203e20a2309fd6c00635186a4892
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-04-24 17:48:36 +0530

    design completed

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 50,
@@ -34,50 +35,36 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            SizedBox(
+                height: 20,
+                width: 120,
+                child: Divider(color: Colors.teal.shade200)),
             Card(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Icon(Icons.phone, color: Colors.teal),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "+94 70 154 9225",
-                      style: TextStyle(
-                          color: Colors.teal,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 13),
-                    )
-                  ],
-                ),
-              ),
-            ),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    "+94 70 154 9225",
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 15),
+                  ),
+                )),
             Card(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Icon(Icons.email, color: Colors.teal),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "lakshansandaruwan1998@gmail.com",
-                      style: TextStyle(
-                          color: Colors.teal,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 13),
-                    )
-                  ],
-                ),
-              ),
-            ),
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal),
+                  title: Text(
+                    "lakshansandaruwan1998@gmail.com",
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 15),
+                  ),
+                )),
           ],
         )),
       ),
