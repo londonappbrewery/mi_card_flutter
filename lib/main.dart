a26967e1b1443fffb93289610d974dda44a15cb3
KekePhasha <61992331+KekePhasha@users.noreply.github.com>
2022-07-28 13:44:59 +0200

    Hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
-//spodjfpsdfsdkfmsdf
+
+class MyApp extends StatelessWidget {
+  // const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Center(child: Text('data')),
+        ),
+        body: Container(),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(
+          Icons.add,
+        )),
+      ),
+    );
+  }
+}
