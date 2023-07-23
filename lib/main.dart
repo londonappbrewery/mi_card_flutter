0037496194fd5c0a7887c5af92d5b1735b7841a4
swpinchen <swpinchen@outlook.com>
2023-07-23 18:54:28 +0900

    Added comtainer with margin and padding

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,20 @@ class MyApp extends StatelessWidget {
     // return const Placeholder();
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.yellow,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            // Containers can only have single child
+            child: Container(
+              height: 100.0,
+              width: 100.0,
+              // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              // margin: EdgeInsets.fromLTRB(30, 10, 50, 20),
+              margin: EdgeInsets.only(left: 30),
+              padding: EdgeInsets.all(20.0),
+              color: Colors.white,
+              child: Text('Hello'),
+            )
+        ),
       ),
     );
   }
