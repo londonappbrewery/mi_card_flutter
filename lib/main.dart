4aed6893365ab153112b5304b94a84d657f0f4f2
Mongkoclhai Tue <37042+MongkolchaiTue@users.noreply.github.com>
2024-01-19 00:54:59 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,94 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 55.0,
+                backgroundImage: AssetImage('images/i_am_rich_app_icon.png'),
+              ),
+              Text(
+                'สวัสดี, Angela Yu',
+                style: TextStyle(
+                  fontFamily: 'NotoSansThai',
+                  fontSize: 20.0,
+                  color: Colors.blue.shade100,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.home,
+                      size: 40.0,
+                      color: Colors.teal.shade600,
+                    ),
+                    Text(
+                      'Tel.+66 123 456 789',
+                      style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        letterSpacing: 2.5,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    Text(
+                        'angela@email.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Pacifico'
+                      ),
+                    ),
+                  ],
+                )
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
